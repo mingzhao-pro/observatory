1460c53093c921078142527a720c9493b3f1b4ab
Ming ZHAO <mingzhao.life@gmail.com>
2017-07-21 05:12:30 +0200

    finally finished!!!!

--- src/main/scala/observatory/Interaction2.scala
+++ src/main/scala/observatory/Interaction2.scala
@@ -1,5 +1,7 @@
 package observatory
 
+import observatory.LayerName.{Deviations, Temperatures}
+
 /**
   * 6th (and last) milestone: user interface polishing
   */
@@ -9,7 +11,27 @@ object Interaction2 {
     * @return The available layers of the application
     */
   def availableLayers: Seq[Layer] = {
-    ???
+    val white = (60.0, Color(255, 255, 255))
+    val red = (32.0, Color(255, 0, 0))
+    val yellow = (12.0, Color(255, 255, 0))
+    val sky = (0.0, Color(0, 255, 255))
+    val blue = (-15.0, Color(0, 0, 255))
+    val violet = (-27.0, Color(255, 0, 255))
+    val purple = (-50.0, Color(33, 0, 107))
+    val black = (-60.0, Color(0, 0, 0))
+    val colorScaleForTemp = Seq(white, red, yellow, sky, blue, violet, purple, black)
+    val tempLayer = Layer(Temperatures, colorScaleForTemp, Range(1975, 1989, 1))
+
+    val black_ = (7.0, Color(0, 0, 0))
+    val red_ = (4.0, Color(255, 0, 0))
+    val yellow_ = (2.0, Color(255, 255, 0))
+    val white_ = (0.0, Color(255, 255, 255))
+    val sky_ = (-2.0, Color(0, 255, 255))
+    val blue_ = (-7.0, Color(0, 0, 255))
+    val colorScaleForDev = Seq(black_, red_, yellow_, white_, sky_, blue_)
+    val devLayer = Layer(Deviations, colorScaleForDev, Range(1990, 2015, 1))
+
+    Seq(tempLayer, devLayer)
   }
 
   /**
@@ -17,7 +39,7 @@ object Interaction2 {
     * @return A signal containing the year bounds corresponding to the selected layer
     */
   def yearBounds(selectedLayer: Signal[Layer]): Signal[Range] = {
-    ???
+    Signal(selectedLayer().bounds)
   }
 
   /**
@@ -29,7 +51,10 @@ object Interaction2 {
     *         in the `selectedLayer` bounds.
     */
   def yearSelection(selectedLayer: Signal[Layer], sliderValue: Signal[Int]): Signal[Int] = {
-    ???
+    val range = yearBounds(selectedLayer)
+    Signal(if(range().contains(sliderValue())) sliderValue()
+    else if(range().start > sliderValue()) range().start
+    else range().end)
   }
 
   /**
@@ -38,7 +63,7 @@ object Interaction2 {
     * @return The URL pattern to retrieve tiles
     */
   def layerUrlPattern(selectedLayer: Signal[Layer], selectedYear: Signal[Int]): Signal[String] = {
-    ???
+    Signal("target/" + selectedLayer().layerName.id + "/" + selectedYear())
   }
 
   /**
@@ -47,7 +72,8 @@ object Interaction2 {
     * @return The caption to show
     */
   def caption(selectedLayer: Signal[Layer], selectedYear: Signal[Int]): Signal[String] = {
-    ???
+    val name = Signal(selectedLayer().layerName.id)
+    Signal(name().substring(0, 1).toUpperCase + name().substring(1) + " (" + selectedYear() + ")")
   }
 
 }
--- src/test/scala/observatory/Interaction2Test.scala
+++ src/test/scala/observatory/Interaction2Test.scala
@@ -8,4 +8,12 @@ import org.scalatest.prop.Checkers
 @RunWith(classOf[JUnitRunner])
 class Interaction2Test extends FunSuite with Checkers {
 
+  test("") {
+    import observatory.{Interaction2, Signal}
+
+    val layers = Interaction2.availableLayers
+
+    for(x <- layers)
+      println(Interaction2.yearSelection(Signal(x), Signal(2010))())
+  }
 }
